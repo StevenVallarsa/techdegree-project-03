e386248bd5689f110917b00ee78a4e67512466bf
StevenVallarsa <orionsjv@gmail.com>
2022-06-22 20:25:25 -0400

    Added functionality to Basic Info section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("HERE");
+// make "name" field selected upon load
+const name = document.getElementById("name");
+name.focus();
+
+const otherJobRole = document.getElementById("other-job-role");
+otherJobRole.style.display = "none";
+
+const title = document.getElementById("title");
+
+title.addEventListener("change", e => {
+  if (e.target.value === "other") {
+    otherJobRole.style.display = "block";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+});
