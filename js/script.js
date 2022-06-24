65fda76d30198b5554db39b252187abe65ed2724
StevenVallarsa <orionsjv@gmail.com>
2022-06-24 03:55:04 -0400

    more validation setup

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ const title = document.getElementById("title");
 const total = document.getElementById("activities-cost");
 const allActivities = document.querySelectorAll("#activities-box input");
 const activities = document.getElementById("activities");
+const payPal = document.getElementById("paypal");
+const bitCoin = document.getElementById("bitcoin");
 
 // make "name" field selected upon load
 name.focus();
+
+// hide PayPal and BitCoin text boxes on load
+payPal.style.display = "none";
+bitCoin.style.display = "none";
 
 /**
  * Validators
@@ -33,13 +39,23 @@ function isEmailValid() {
 }
 
 function hasRegistered() {
-  return Array.from(allActivities).some(activity => activity.checked);
+  const isValid = Array.from(allActivities).some(activity => activity.checked);
+  if (!isValid) {
+    activities.classList.add("not-valid");
+  } else {
+    activities.classList.remove("not-valid");
+  }
+  return;
 }
 
 function isCreditCardValid() {
   const zip = document.getElementById("zip");
   const cvv = document.getElementById("cvv");
   const cardNumber = document.getElementById("cc-num");
+
+  const validZip = /\d{5}/.test(zip.value);
+  const validCVV = /\d{3}/.test(cvv.value);
+  const validCCNumber = /\d{13,16}/.test(cardNumber.value);
 
   return /\d{5}/.test(zip.value) && /\d{3}/.test(cvv.value) && /\d{13,16}/.test(cardNumber.value);
 }
@@ -95,7 +111,16 @@ const creditCard = document.getElementById("credit-card");
 payment.addEventListener("change", e => {
   if (e.target.value !== "credit-card") {
     creditCard.style.display = "none";
+    if (e.target.value === "paypal") {
+      bitCoin.style.display = "none";
+      payPal.style.display = "block";
+    } else {
+      bitCoin.style.display = "block";
+      payPal.style.display = "none";
+    }
   } else {
+    payPal.style.display = "none";
+    bitCoin.style.display = "none";
     creditCard.style.display = "block";
   }
 });
@@ -116,20 +141,18 @@ checkboxes.forEach(checkbox => {
 // validation on submit
 const form = document.querySelector("form");
 form.addEventListener("submit", e => {
-  let isValidated = true;
-  if (!isNameValid() || !isEmailValid() || !hasRegistered()) {
-    isValidated = false;
+  const isNameValidCheck = isNameValid();
+  const isEmailValidCheck = isEmailValid();
+  const isRegistrationValidCheck = hasRegistered();
+  let isCreditCardValidCheck = true;
+  if (payment.value === "credit-card") {
+    isCreditCardValidCheck = isCreditCardValid();
   }
 
-  if (payment.value === "credit-card") {
-    if (!isCreditCardValid()) {
-      isValidated = false;
-    }
-  }
-  if (!isValidated) {
-    console.log("NOPE");
+  const everythingIsValid = isNameValidCheck && isEmailValidCheck && isRegistrationValidCheck && isCreditCardValidCheck;
+
+  if (!everythingIsValid) {
     e.preventDefault();
-  } else {
-    console.log("YOU MADE IT");
+    console.log("NOPE");
   }
 });
